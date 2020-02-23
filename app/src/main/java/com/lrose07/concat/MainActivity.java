99630d5dab515b14a28cc811d1934e1585f92916
Lauren Rose <larose@radford.edu>
2020-02-23 08:08:39 -0500

    update create new event

--- app/src/main/java/com/lrose07/concat/MainActivity.java
+++ app/src/main/java/com/lrose07/concat/MainActivity.java
@@ -198,7 +198,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUniqueFlag(boolean bool) {
+        System.out.println("uf: " + uniqueFlag);
         uniqueFlag = bool;
+        System.out.println("uf: " + uniqueFlag);
     }
 
     private void checkCurrentEventNull() {
